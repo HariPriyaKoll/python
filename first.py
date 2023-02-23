428d0cc96b94436552190b35d955adb9a18c827a
kusuma41 <kusuma122.kolli@gmail.com>
2023-02-23 10:32:47 +0530

    first python file

---
+++ first.py
@@ -0,0 +1,7 @@
+import requests 
+import json
+
+response = requests.get('https://api.zippopotam.us/us/90210')
+data = json.loads(response.text)
+print(data['country'])
+print(data['places'][0]['place name'])
