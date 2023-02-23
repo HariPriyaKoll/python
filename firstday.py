a6e1e4194f51c76822fea450df18ccdb67f9206b
kusuma41 <kusuma122.kolli@gmail.com>
2023-02-23 15:27:37 +0530

    firts day completed

---
+++ firstday.py
@@ -0,0 +1,49 @@
+print("hello" + "vamsi")
+print("hello" + " " + "vamsi")
+print("heloo vamsi\nhello krishan")
+vamsi = 5
+krishna = 10 + vamsi
+print(krishna)
+
+'''
+When you run your program, it should print the following:
+
+Day 1 - String Manipulation
+String Concatenation is done with the "+" sign.
+e.g. print("Hello " + "world")
+New lines can be created with a backslash and n.
+'''
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+'''
+Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+'''
+name = input("what is your name?")
+print(len(name))
+
+'''
+Write a program that switches the values stored in the variables a and b.
+'''
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
